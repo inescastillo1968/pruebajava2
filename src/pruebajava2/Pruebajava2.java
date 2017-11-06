b6c1cfbadc5d589c168c98f6fcd39e2c4cf9d99e
inescastillo1968 <inesmayo12345@hotmail.com>
2017-11-06 18:28:12 -0300

    creacion encabezado multilinea pruebajava2

---
+++ src/pruebajava2/Pruebajava2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+
+/********************************************************************
+ nombre marjorie castillo
+ fecha 06 11 2017
+ clase pruebajava2
+ responsabilidad 
+ *
+ * @author LAB10
+ */
+public class Pruebajava2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
