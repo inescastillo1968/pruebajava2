d17725d1a63957049fb401588f6614371b7603e7
inescastillo1968 <inesmayo12345@hotmail.com>
2017-11-06 18:52:06 -0300

    creacion programa por ventana  con estructura switch

--- src/pruebajava2/Pruebajava2.java
+++ src/pruebajava2/Pruebajava2.java
@@ -4,12 +4,12 @@
  * and open the template in the editor.
  */
 package pruebajava2;
-
+import javax.swing.JOptionPane;
 /********************************************************************
  nombre marjorie castillo
  fecha 06 11 2017
  clase pruebajava2
- responsabilidad 
+ responsabilidad clase principal
  *
  * @author LAB10
  */
@@ -18,8 +18,50 @@ public class Pruebajava2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        float mes = Float.parseFloat(JOptionPane.showInputDialog("Ingrese mes"));
+        switch(Math.round (mes))
+        {
+        case 1:
+          JOptionPane.showMessageDialog(null, "enero");     
+           break;
+           case 2:
+          JOptionPane.showMessageDialog(null, "febrero");     
+           break;
+            case 3:
+          JOptionPane.showMessageDialog(null, "marzo");     
+           break;
+             case 4:
+          JOptionPane.showMessageDialog(null, "abril");     
+           break;
+            case 5:
+          JOptionPane.showMessageDialog(null, "mayo");     
+           break;
+           case 6:
+          JOptionPane.showMessageDialog(null, "junio");     
+           break;
+            case 7:
+          JOptionPane.showMessageDialog(null, "julio");     
+           break;
+             case 8:
+          JOptionPane.showMessageDialog(null, "agosto");     
+           break;
+             case 9:
+          JOptionPane.showMessageDialog(null, "septiembre");     
+           break;
+           case 10:
+          JOptionPane.showMessageDialog(null, "octubre");     
+           break;
+            case 11:
+          JOptionPane.showMessageDialog(null, "noviembre");     
+           break;
+             case 12:
+          JOptionPane.showMessageDialog(null, "diciembre");     
+           break;
     }
     
 }
+    
+        
+    }
