dd91999eeae381d1fd1f4672b5db9ab4ab170d7d
inescastillo1968 <inesmayo12345@hotmail.com>
2017-11-06 20:02:30 -0300

    creacion programa que pide la contraseña del usuario

---
+++ src/crearprogramadecontraseña.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/****************************************
+ * nombre marjorie castillo
+ fecha 06 11 2017
+ clase pruebajava2
+ responsabilidad clase 2
+ *
+ * @author LAB10
+ */
+import javax.swing.JOptionPane;
+public class crearprogramadecontraseña 
+{
+   public static void main(String[] args)
+   {
+    int 7777;
+     
+     int = Integer.parseInt(JOptionPane.showInputDialog("ingrese contraseña"));
+ 
+     if(int==7777)
+     {
+       int = Integer.parseInt(JOptionPane.showInputDialog("contraseña definida"));
+             
+     }
+     else 
+     {
+     int = Integer.parseInt(JOptionPane.showInputDialog("cuenta bloqueada"));    
+     }
+    
+   }
+           
+           
+           
+           
+           
+           
+           
+}
